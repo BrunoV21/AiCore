ed485c253a48d9603e0437bb9c1fe7e06ceaa006
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-01-23 22:52:37 +0000

    feat: implement token counting and tokenizer function for GeminiLlm provider

--- aicore/llm/providers/gemini.py
+++ aicore/llm/providers/gemini.py
@@ -1,4 +1,29 @@
 from aicore.llm.providers.openai import OpenAiLlm
+from pydantic import model_validator
+from google.genai import Client
+from functools import partial
+from typing import List, Self
 
 class GeminiLlm(OpenAiLlm):
     base_url :str="https://generativelanguage.googleapis.com/v1beta/openai/"
+
+    @staticmethod
+    def gemini_count_tokens(contents :str, client :Client, model :str)->List[int]:
+        response = client.models.count_tokens(
+            contents=contents,
+            model=model
+        )
+        return [i for i in range(response.total_tokens)] if response.total_tokens else []
+
+    @model_validator(mode="after")
+    def pass_gemini_tokenizer_fn(self)->Self:
+        _client = Client(
+            api_key=self.config.api_key
+        )
+        self.tokenizer_fn = partial(
+            self.gemini_count_tokens,
+            client=_client,
+            model=self.config.model
+        )
+
+        return self
