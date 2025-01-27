bc318de9d9442f18195c0396dcf2a6c5566307e9
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-01-27 23:36:57 +0000

    feat: add support for reasoner providers and models in LlmConfig, including example configuration

--- aicore/const.py
+++ aicore/const.py
@@ -1,3 +1,7 @@
 DEFAULT_CONFIG_PATH = "./config/config.yml"
 
+SUPPORTED_REASONER_PROVIDERS = ["groq"]
+
+SUPPORTED_REASONER_MODELS = ["deepseek-r1-distill-llama-70b"]
+
 DEFAULT_ENCODING = "utf8"
--- aicore/llm/config.py
+++ aicore/llm/config.py
@@ -1,5 +1,7 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, Union
 from pydantic import BaseModel, field_validator
+
+from aicore.const import SUPPORTED_REASONER_PROVIDERS, SUPPORTED_REASONER_MODELS
 
 class LlmConfig(BaseModel):
     provider :Literal["gemini", "groq", "mistral", "nvidia", "openai"]
@@ -8,11 +10,17 @@ class LlmConfig(BaseModel):
     base_url :Optional[str]=None
     temperature :float=0
     max_tokens :int=124000
+    reasoner :Union["LlmConfig", None]=None
 
     @field_validator("temperature")
     @classmethod
     def ensure_temperature_lower_than_unit(cls, temperature :float)->float:
         assert 0 <= temperature <= 1, "temperature should be between 0 and 1"
         return temperature
-
     
+    @field_validator("reasoner", mode="after")
+    @classmethod
+    def ensure_valid_reasoner(cls, reasoner :"LlmConfig")->"LlmConfig":
+        assert reasoner.provider in SUPPORTED_REASONER_PROVIDERS, f"{reasoner.provider} is not supported as a reasoner provider. Supported providers are {SUPPORTED_REASONER_PROVIDERS}"
+        assert reasoner.model in SUPPORTED_REASONER_MODELS, f"{reasoner.model} is not supported as a reasoner model. Supported models are {SUPPORTED_REASONER_MODELS}"
+        return reasoner
