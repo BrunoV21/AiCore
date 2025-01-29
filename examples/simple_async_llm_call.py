38420ccf605931bb8cc0e416a394fbb7bbb1f15c
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-01-29 23:47:30 +0000

    feat: add example scripts for async LLM calls and update synchronous call example

--- examples/simple_async_llm_call.py
+++ examples/simple_async_llm_call.py
@@ -1,9 +1,13 @@
 from aicore.config import Config
 from aicore.llm import Llm
 import os
+import asyncio
 
 if __name__ == "__main__":
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
     os.environ["CONFIG_PATH"] = "./config/config.yml"
     config = Config.from_yaml()
     llm = Llm.from_config(config.llm)
-    llm.complete("Tell me the story of how OpenAi became ClosedAi")
+    asyncio.run(llm.acomplete("how much us 8 + 9 - 15"))
