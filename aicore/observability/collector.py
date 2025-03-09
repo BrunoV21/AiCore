9fe3e7b1954293a21d031cddedfcb013c69a67bf
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-03-09 20:20:05 +0000

    feat: enhance LlmOperationRecord with default values and non-null validation; add method to load data from file using Polars

--- aicore/observability/collector.py
+++ aicore/observability/collector.py
@@ -4,28 +4,36 @@ from pydantic import BaseModel,  RootModel, Field, field_validator, computed_fie
 from typing import Dict, Any, Optional, Callable, List, Union, Literal, Self
 from datetime import datetime
 from pathlib import Path
+import polars as pl
 import ulid
 import json
 import os
 
 class LlmOperationRecord(BaseModel):
     """Data model for storing information about a single LLM operation."""
-    session_id: Optional[str] = None
-    agent_id: Optional[str] = None
+    session_id: Optional[str] = ""
+    agent_id: Optional[str] = ""
     operation_id: str = Field(default_factory=ulid.ulid)
-    timestamp: Optional[str] = None
+    timestamp: Optional[str] = ""
     operation_type: Literal["completion", "acompletion"]
     provider :str
     input_tokens: Optional[int] = 0
     output_tokens: Optional[int] = 0
     cost: Optional[float] = 0
     latency_ms: float
-    error_message: Optional[str] = None
+    error_message: Optional[str] = ""
     completion_args: Union[Dict[str, Any], str]
-    response: Optional[Union[str, Dict, List]] = None
+    response: Optional[Union[str, Dict, List]] = ""
     
     class Config:
         arbitrary_types_allowed = True
+
+    @field_validator(*["session_id", "agent_id", "timestamp", "error_message", "response"])
+    @classmethod
+    def ensure_non_nulls(cls, value :Optional[str]=None)->str:
+        if value is None:
+            return ""
+        return value
     
     @field_validator("response")
     @classmethod
@@ -47,7 +55,7 @@ class LlmOperationRecord(BaseModel):
         
     @model_validator(mode="after")
     def init_timestamp(self)->Self:
-        if self.timestamp is None:
+        if not self.timestamp:
             self.timestamp = datetime.now().isoformat()
         return self
         
@@ -61,36 +69,36 @@ class LlmOperationRecord(BaseModel):
     
     @computed_field
     def model(self)->str:
-        return self.completion_args.get("model")
+        return self.completion_args.get("model", "")
     
     @computed_field
     def temperature(self)->float:
-        return self.completion_args.get("temperature")
+        return self.completion_args.get("temperature", "")
     
     @computed_field
     def max_tokens(self)->int:
-        return self.completion_args.get("max_tokens")
+        return self.completion_args.get("max_tokens", 0)
     
     @computed_field
     def system_prompt(self)->Optional[str]:
         for msg in self.messages:
             if msg.get("role") == "system": #or dev open ai o -series?
-                return msg.get("content")
-        return None
+                return msg.get("content", "")
+        return ""
 
     @computed_field
     def assistant_message(self)->Optional[str]:
         for msg in self.messages[::-1]:
             if msg.get("role") == "assistant": #or dev open ai o -series?
-                return msg.get("content")
-        return None
+                return msg.get("content", "")
+        return ""
 
     @computed_field
     def user_prompt(self)->Optional[str]:
         for msg in self.messages[::-1]:
             if msg.get("role") == "user": #or dev open ai o -series?
-                return msg.get("content")
-        return None
+                return msg.get("content", "")
+        return ""
     
     @computed_field
     def history_messages(self)->Optional[str]:
@@ -136,7 +144,7 @@ class LlmOperationRecord(BaseModel):
             "cost": self.cost,
             "latency_ms": self.latency_ms,
             "error_message": self.error_message,
-            "completion_args": self.completion_args
+            "completion_args": json.dumps(self.completion_args, indent=4)
         }
 
 class LlmOperationCollector(RootModel):
@@ -211,12 +219,20 @@ class LlmOperationCollector(RootModel):
 
     @classmethod
     def fom_observable_storage_path(cls, storage_path: Optional[str] = None)->"LlmOperationCollector":
-        cls = cls()        
+        cls = cls()
         env_path = os.environ.get("OBSERVABILITY_DATA_DEFAULT_FILE")
         if storage_path:
             cls.storage_path = storage_path
         elif env_path:
             cls.storage_path = env_path
         else:
-            cls.storage_path = Path(DEFAULT_OBSERVABILITY_DIR) / DEFAULT_OBSERVABILITY_FILE        
+            cls.storage_path = Path(DEFAULT_OBSERVABILITY_DIR) / DEFAULT_OBSERVABILITY_FILE
         return cls
+    
+    @classmethod
+    def polars_from_file(cls, storage_path:  Optional[str] = None)->pl.DataFrame:
+        obj = cls.fom_observable_storage_path(storage_path)
+        if os.path.exists(obj.storage_path):
+            with open(obj.storage_path, 'r', encoding=DEFAULT_ENCODING) as f:
+                obj = cls(root=json.loads(f.read()))
+        return pl.from_dicts(obj.model_dump())
