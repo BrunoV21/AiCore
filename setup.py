2112a2cd4458b0b01fa4fa7874300acdd6242113
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-04-02 18:30:26 +0100

    chore: bump version to 0.1.88 in setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ extras_require = {
 
 setuptools.setup(
     name="core-for-ai",
-    version="0.1.87",
+    version="0.1.88",
     author="Bruno V.",
     author_email="bruno.vitorino@tecnico.ulisboa.pt",
     description="A unified interface for interacting with various LLM and embedding providers, with observability tools.",
