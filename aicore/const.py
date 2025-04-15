c9c9bd0d8eb9633f68e885860ea798ee79663adc
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-04-15 09:49:00 +0100

    feat: add gemini-2.5-pro-preview-03-25 to CUSTOM_MODELS list

--- aicore/const.py
+++ aicore/const.py
@@ -11,7 +11,8 @@ DEFAULT_LOGS_DIR = os.getenv("LOGS_PATH") or "logs"
 CUSTOM_MODELS = [
     "gemini-2.0-flash-exp",
     "gemini-2.0-flash-thinking-exp-01-21",
-    "gemini-2.5-pro-exp-03-25"
+    "gemini-2.5-pro-exp-03-25",
+    "gemini-2.5-pro-preview-03-25"
 ]
 
 try:
