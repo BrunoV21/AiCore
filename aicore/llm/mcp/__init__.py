40892ed9848684ab4d971c2bfb276701ad36063b
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-05-02 01:01:42 +0100

    Refactor MCP client structure: consolidate client initialization, enhance server management, and introduce new models for server configuration and tool schemas.

--- aicore/llm/mcp/__init__.py
+++ aicore/llm/mcp/__init__.py
@@ -1,35 +1,5 @@
-"""
-include support to load mcp clients from config in config
-see exampe here https://mcpservers.org/official
+from aicore.llm.mcp.client import MCPClient
 
-### 
-
-### Claude Desktop like config:
-npm install -g agentql-mcp 
-+ to claude_desktop_config.json
-{
-  "mcpServers": {
-    "agentql": {
-      "command": "npx",
-      "args": ["-y", "agentql-mcp"],
-      "env": {
-        "AGENTQL_API_KEY": "YOUR_API_KEY"
-      }
-    }
-  }
-}
-
----
-{
-  "mcpServers": {
-    "@21st-dev/magic": {
-      "command": "npx",
-      "args": ["-y", "@21st-dev/magic@latest", "API_KEY=\"your-api-key\""]
-    }
-  }
-}
-
-mcp_config_json -> load from default location on root or from
-
-meed to cover loading of all mcp servers + descriptions and incorporate them into plan the llm can decide which one to choose
-"""
+__all__ = [
+    MCPClient
+]
