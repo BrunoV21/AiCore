58ac18d754905f052f474f5aaf8f08dd02f53489
bruno_v <120278082+BrunoV21@users.noreply.github.com>
2025-05-08 16:45:44 +0100

    version bump

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ extras_require = {
 
 setuptools.setup(
     name="core-for-ai",
-    version="0.1.91",
+    version="0.1.92",
     author="Bruno V.",
     author_email="bruno.vitorino@tecnico.ulisboa.pt",
     description="A unified interface for interacting with various LLM and embedding providers, with observability tools.",
