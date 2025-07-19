b900903b54dad06bd450b99f5f264fa205b4ee65
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-07-19 23:37:23 +0100

    Refactor imports in models.py to streamline dependencies

--- aicore/observability/models.py
+++ aicore/observability/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, String, Integer, Float, Boolean, Text, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
